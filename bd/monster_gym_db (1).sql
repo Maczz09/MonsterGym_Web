138658f96511073b008cea62a7d40c8b6698cf4e
Maczz09 <u22201741@utp.edu.pe>
2024-09-27 16:43:29 -0500

    Agregada carpeta bd con archivo monster_gym_db.sql

---
+++ bd/monster_gym_db (1).sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-09-2024 a las 23:37:13
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `monster_gym_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `miembros`
+--
+-- Error leyendo la estructura de la tabla monster_gym_db.miembros: #1932 - Table &#039;monster_gym_db.miembros&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla monster_gym_db.miembros: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `monster_gym_db`.`miembros`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+-- Error leyendo la estructura de la tabla monster_gym_db.roles: #1932 - Table &#039;monster_gym_db.roles&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla monster_gym_db.roles: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `monster_gym_db`.`roles`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_membresia`
+--
+-- Error leyendo la estructura de la tabla monster_gym_db.tipo_membresia: #1932 - Table &#039;monster_gym_db.tipo_membresia&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla monster_gym_db.tipo_membresia: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `monster_gym_db`.`tipo_membresia`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+-- Error leyendo la estructura de la tabla monster_gym_db.usuarios: #1932 - Table &#039;monster_gym_db.usuarios&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla monster_gym_db.usuarios: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `monster_gym_db`.`usuarios`&#039; en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
